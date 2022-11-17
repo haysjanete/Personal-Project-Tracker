f985ace44a549404370332c823ae444034973556
haysjanete <74339100+haysjanete@users.noreply.github.com>
2022-11-17 15:02:32 -0600

    added more react code for sign in and signout page

--- ghi/src/LoginSignupPage.js
+++ ghi/src/LoginSignupPage.js
@@ -19,49 +19,47 @@ window.onload = function () {
   secondForm.addEventListener("submit", (e) => e.preventDefault());
 };
 
-class LoginSignupPage extends React.Component {
-  render() {
-    return (
-      <div className="container right-panel-active">
-        ///* Sign Up */
-        <div className="container__form container--signup">
-          <form action="#" className="form" id="form1">
-            <h2 className="form__title">Sign Up</h2>
-            <input type="text" placeholder="Name" className="input" />
-            <input type="tel" placeholder="Phone Number" className="input" />
-            <input type="email" placeholder="Email" className="input" />
-            <input type="text" placeholder="Username" className="input" />
-            <input type="password" placeholder="Password" className="input" />
-            <button className="btn">Sign Up</button>
-          </form>
-        </div>
-        ///* Sign In */
-        <div className="container__form container--signin">
-          <form action="#" className="form" id="form2">
-            <h2 className="form__title">Sign In</h2>
-            <input type="email" placeholder="Username" className="input" />
-            <input type="password" placeholder="Password" className="input" />
-            <button className="btn">Sign In</button>
-          </form>
-        </div>
-        ///* Overlay */
-        <div className="container__overlay">
-          <div className="overlay">
-            <div className="overlay__panel overlay--left">
-              <button className="btn" id="signIn">
-                Sign In
-              </button>
-            </div>
-            <div className="overlay__panel overlay--right">
-              <button className="btn" id="signUp">
-                Sign Up
-              </button>
-            </div>
+function LoginSignupPage() {
+  return (
+    <div className="container right-panel-active">
+      ///* Sign Up */
+      <div className="container__form container--signup">
+        <form action="#" className="form" id="form1">
+          <h2 className="form__title">Sign Up</h2>
+          <input type="text" placeholder="Name" className="input" />
+          <input type="tel" placeholder="Phone Number" className="input" />
+          <input type="email" placeholder="Email" className="input" />
+          <input type="text" placeholder="Username" className="input" />
+          <input type="password" placeholder="Password" className="input" />
+          <button className="btn">Sign Up</button>
+        </form>
+      </div>
+      ///* Sign In */
+      <div className="container__form container--signin">
+        <form action="#" className="form" id="form2">
+          <h2 className="form__title">Sign In</h2>
+          <input type="email" placeholder="Username" className="input" />
+          <input type="password" placeholder="Password" className="input" />
+          <button className="btn">Sign In</button>
+        </form>
+      </div>
+      ///* Overlay */
+      <div className="container__overlay">
+        <div className="overlay">
+          <div className="overlay__panel overlay--left">
+            <button className="btn" id="signIn">
+              Sign In
+            </button>
+          </div>
+          <div className="overlay__panel overlay--right">
+            <button className="btn" id="signUp">
+              Sign Up
+            </button>
           </div>
         </div>
       </div>
-    );
-  }
+    </div>
+  );
 }
 
 export default LoginSignupPage;
